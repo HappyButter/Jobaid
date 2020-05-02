a1de87c7b2919f3dd104c0e2c6a8cc170958fc82
Adrian Furman <fadikk367@gmail.com>
2020-05-02 23:11:56 +0200

    Create django apps, .gitignore and templates & static folders

---
+++ salary_prediction/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse # just temporary until templates will be created
+
+def home(request):
+    return HttpResponse('<h1>salary prediction page</h1>')
---
+++ statistics_and_charts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StatisticsAndChartsConfig(AppConfig):
+    name = 'statistics_and_charts'
