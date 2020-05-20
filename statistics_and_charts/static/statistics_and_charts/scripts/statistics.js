12a05a044eebedf417baee9b7837aae43bbaa890
Adrian Furman <fadikk367@gmail.com>
2020-05-20 19:55:06 +0200

    Add example endpoint for json chart data

--- statistics_and_charts/static/statistics_and_charts/scripts/statistics.js
+++ statistics_and_charts/static/statistics_and_charts/scripts/statistics.js
@@ -1,16 +1,49 @@
 console.log("hello there from statistics script");
 
 const chartDiv = document.querySelector('.chart__example');
+// Plotly.newPlot(chartDiv, {}, {});
 
-const data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] 
-}];
-
-const layout = {
-    margin: { 
-        t: 0 
-    }
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
 }
 
-Plotly.newPlot( chartDiv, data, layout);
+const createPlot = async (url, chartDiv) => {
+    const statistics = await fetchData(url);
+    console.log(statistics);
+
+    const values = Object.values(statistics);
+    const labels = Object.keys(statistics);
+
+    const total = values.reduce((sum, value) => sum + value, 0);
+    const normalized = values.map(value => value/total * 100)
+
+    // const data = [{
+    //     values,
+    //     labels,
+    //     type: 'pie'
+    // }];
+    const data = [
+        {
+            x: labels,
+            y: normalized,
+            histfunc: 'count',
+            opacity: 0.8,
+            type: 'bar',
+            marker: {
+                color: 'darkblue'
+            }
+        }
+    ]
+
+    const layout = {
+        margin: { 
+            t: 20 
+        }
+    }
+
+    Plotly.newPlot(chartDiv, data, layout);
+}
+
+
+createPlot('http://127.0.0.1:8000/statistics/example/', chartDiv);
--- statistics_and_charts/urls.py
+++ statistics_and_charts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.statistics, name="statistics_and_charts-home"),
+    path("example/", views.example, name="statistics_example")
 ]
