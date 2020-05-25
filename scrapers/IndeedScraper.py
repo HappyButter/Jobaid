ac9ae98ffcd045a250e6176861b8c57106c2383e
MeetU <mateusz.g64@interia.pl>
2020-05-26 00:36:24 +0200

    IndeedScraper final touches

--- scrapers/IndeedScraper.py
+++ scrapers/IndeedScraper.py
@@ -4,79 +4,63 @@ import json
 import math
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from hashlib import blake2b
 from bs4 import BeautifulSoup
+from datetime import date, timedelta
 
 
-languages = ['C#',
-             'C++',
-             'Python',
-             'Javascript',
-             'Typescript',
-             'PhP',
-             'Ruby on rails',
-             'Html5',
-             'Html',
-             'CSS3',
-             'SCSS',
-             'SASS',
-             'CSS',
-             'Kotlin',
-             'Swift',
-             'Flutter',
-             'Scala',
-             'Rust',
-             'Vba',
-             'Ruby',
-             'Objective-C',
-             'NoSQL',
-             'Dart',
-             'Golang',
-             'PhP 7',
-             'Julia',
-             'Elixir',
-             'Erlang']
+DEST_FOLDER = "scraped_data/"
+SOURCE_FOLDER = "info/"
+
+languages = [
+            "Assembler",
+            "Pascal",
+            "Elixir",
+            "CSS3",
+            "HTML",
+            "NoSQL",
+            "Python",
+            "Ruby",
+            "C#",
+            "Fortran",
+            "Lisp",
+            "Matlab"
+            "Objective-C",
+            "HTML5",
+            "SCSS",
+            "Erlang",
+            "PHP",
+            "Kotlin",
+            "Rust",
+            "Flutter",
+            "Julia",
+            "CSS",
+            "C++",
+            "Golang",
+            "TypeScript",
+            "JavaScript",
+            "VBA",
+            "Dart"
+        ]
 
 exceptional_languages = ["C",
                          "R",
-                         "Lua",
+                         "Swift",
+                         "Scala",
                          "Go",
                          "Java",
                          "SQL",
                          "Lua"]
 
-technologies = ["Android", "Maven", "CMake", "Node.js", "Vue.js",
-                "MongoDB", "autostar", "TCP/IP", "Scrum",
-                "Deep Learning", "Bitbucket", "Jenkins",
-                "Jira", "Confluence", "Docker", "Mesos", "C++11", "C++17", "C++14",
-                "C++20", "C++11/14/17", "Kubernetes", "SDN/NFV",
-                "Bamboo", "Windows Server", "SQL Server", "JQuery", "Linux",
-                "Angular", "HTML5", "ASP.NET", "CSS", ".NET", "DevOps",
-                ".NET Core", "SaaS", "Perforce", "Unity2D", "Unity3D",
-                "Unity", "Unreal Engine", "PostgreSQL", "Unix", "MySQL", "Mercirual",
-                "XML", "SpringBoot", "Spring", "automated tests", "writing tests",
-                "Tomcat", "Hibernate", "GoCD", "Webmethod", "Oracle", "IntelliJ",
-                "WinForms", "Assembler", "Assembly", "Azure", "Hadoop", "OpenCV",
-                "Electron", "Computer Vision", "machine learning"]
 
-exceptional_technologies = ["SQL",
-                            "AI",
-                            "ML",
-                            "OSI",
-                            "AWS",
-                            "SED",
-                            "AWK",
-                            "Qt",
-                            "Git",
-                            "CI",
-                            "TDD",
-                            "STL",
-                            "REST",
-                            "TFS",
-                            "Vue",
-                            "React",
-                            "Agile",
-                            "Shell", ]
+def load_data(filename):
+    with open(filename, "r") as file:
+        technologies = json.load(file)
+    return technologies
+
+technologies = load_data(SOURCE_FOLDER + "technologies.json")
+exceptional_technologies = load_data(SOURCE_FOLDER + "exceptional_technologies.json")
 
 contracts_list = [
     "b2b",
@@ -104,17 +88,17 @@ def description_salary_seeker(salary_line):
         else:
             digits = re.findall(r"\d+", salary_line_lower)
             if len(digits) == 4:
-                return [digits[0] + digits[1], digits[2] + digits[3]], None
+                return [int(digits[0] + digits[1]), int(digits[2] + digits[3])], None
             elif len(digits) == 2:
-                return [digits[0], digits[1]], None
+                return [int(digits[0]), int(digits[1])], None
 
     if "b2b" in salary_line_lower:
         salary_line_lower = salary_line_lower.replace("b2b", "")
         digits = re.findall(r"\d+", salary_line_lower)
         if len(digits) == 4:
-            return None, [digits[0] + digits[1], digits[2] + digits[3]]
+            return None, [int(digits[0] + digits[1]), int(digits[2] + digits[3])]
         elif len(digits) == 2:
-            return None, [digits[0], digits[1]]
+            return None, [int(digits[0]), int(digits[1])]
 
     return None, None
 
@@ -128,15 +112,15 @@ def double_contract_salary_handler(text):
 
     if uop_number < b2b_number:
         if len(digits) == 8:
-            return [digits[0] + digits[1], digits[2] + digits[3]
-                    ], [digits[4] + digits[5], digits[6] + digits[7]]
+            return [int(digits[0] + digits[1]), int(digits[2] + digits[3])
+                    ], [int(digits[4] + digits[5]), int(digits[6] + digits[7])]
         else:
             return None, None
 
     if b2b_number < uop_number:
         if len(digits) == 8:
-            return [digits[4] + digits[5], digits[6] + digits[7]
-                    ], [digits[0] + digits[1], digits[2] + digits[3]]
+            return [int(digits[4] + digits[5]), int(digits[6] + digits[7])
+                    ], [int(digits[0] + digits[1]), int(digits[2] + digits[3])]
         else:
             pass
 
@@ -222,7 +206,9 @@ def extract_current_page(pages):
 
 def seeker():
     PATH = "/home/mateusz/browserScrapingDrivers/geckodriver"
-    browser = webdriver.Firefox(executable_path=PATH)
+    opt = Options()
+    # opt.add_argument("--headless")
+    browser = webdriver.Firefox(executable_path=PATH, options=opt)
 
     regex_formula_salary = r"(?i)^.*\b(Salary)\b.*$"
     regex_formula_company_size = r"(?i)^.*\b(Company size)\b.*$"
@@ -233,18 +219,24 @@ def seeker():
                   "https://pl.indeed.com/praca?q=javascript&sort=date&radius=25&start=",
                   "https://pl.indeed.com/praca?q=go+developer&sort=date&radius=25&start="]
 
-    data_json = []
+    data_json = ["java.json", "c++.json", "python.json", "js.json", "go.json"]
 
-    for page_link in page_links:
-        data_json.append(gather_data(
-            browser,
-            page_link,
-            regex_formula_company_size,
-            regex_formula_salary
-        ))
+
+    # for index, page_link in enumerate(page_links):
+    #     save_to_json(gather_data(
+    #         browser,
+    #         page_link,
+    #         regex_formula_company_size,
+    #         regex_formula_salary
+    #     ), DEST_FOLDER + data_json[index])
+    save_to_json(gather_data(
+        browser,
+        page_links[4],
+        regex_formula_company_size,
+        regex_formula_salary
+    ), DEST_FOLDER + data_json[4])
 
     browser.quit()
-    save_to_json(data_json)
 
 
 def gather_data(
@@ -253,16 +245,15 @@ def gather_data(
     regex_formula_company_size,
     regex_formula_salary
 ):
-    data_json = []
-
+    offers = []
     browser.get(page_link + str(0))
     pages = browser.find_element_by_id("searchCountPages").text
     max_page = extract_max_page(pages)
     n_offers  = 0
 
     current_page_number = -1
-    # for offer in range(0, max_page-1, 1): #x 10
-    for offer in range(0, 10, 10):
+    for offer in range(0, 10*(max_page-1), 10): #x 10
+    # for offer in range(0, 10, 10):
 
         browser.get(page_link + str(offer))
         all_jobs = browser.find_elements_by_class_name("result")
@@ -273,14 +264,20 @@ def gather_data(
 
         current_page_number = extract_current_page(pages)
 
-        for job in all_jobs:
-            data_json.append(scrap(job, browser,
+        for index, job in enumerate(all_jobs):
+            job_offer = scrap(job, browser,
                                     regex_formula_company_size,
-                                    regex_formula_salary))
-            n_offers += 1
+                                    regex_formula_salary)
+            if index == 0:
+                offers.append(job_offer)
+                n_offers +=1
+            elif index !=0 and offers[index - 1]["hash"] != job_offer["hash"]:
+                offers.append(job_offer)
+                n_offers += 1
+
 
     print(f"Collected offers: {n_offers}")
-    return data_json
+    return offers
 
 
 def scrap(
@@ -334,8 +331,10 @@ def scrap(
             job_desc = None
 
     # Handling the "contracts" field
-    contracts = handle_contracts(title, job_desc)
-
+    try:
+        contracts = handle_contracts(title, job_desc)
+    except:
+        contracts = None
     # Handling the "salary" field
     try:
         salary = soup.find(class_="salaryText").text.replace("\n", "").strip()
@@ -355,39 +354,63 @@ def scrap(
     if not description_salary:
         description_salary = None
 
-    uop, b2b = description_salary_seeker(description_salary)
-    uop, b2b = decide_where_to_pass_min_max(min, max, uop, b2b, contracts)
+    try:
+        uop, b2b = description_salary_seeker(description_salary)
+        uop, b2b = decide_where_to_pass_min_max(min, max, uop, b2b, contracts)
+    except:
+        uop = None
+        b2b = None
 
+    if uop and (uop[0] < 1000 or uop[0] > 60000 or uop[1] < 1000 or uop[1] > 60000):
+        uop = None
+
+    if b2b and (b2b[0] < 1000 or b2b[0] > 60000 or b2b[1] < 1000 or b2b[1] > 60000):
+        b2b = None
     # Handling the "offer_link" field
-    offer_link = "https://pl.indeed.com/viewjob?jk=" + extract_offer_code(soup)
-
+    try:
+        offer_link = "https://pl.indeed.com/viewjob?jk=" + extract_offer_code(soup)
+    except:
+        offer_link = None
 
     # Handling the "active" field
     try:
         offer_status = browser.find_element_by_class_name("date ").text
+        try:
+            offer_age = int(number_extractor_from_line(offer_status))
+        except:
+            offer_age = 0
     except:
         offer_status = None
+        offer_age = None
 
-    if "30+" in offer_status or offer_status is None:
+    if offer_status == None or "30+" in offer_status:
         active = False
     else:
         active = True
 
     # Handling "company_size" field
-    company_size = find_company_size(regex_formula_company_size, job_desc)
-
+    try:
+        company_size = find_company_size(regex_formula_company_size, job_desc)
+    except:
+        company_size = None
     # Handling the "position level" field
-    position_level = handle_position_level(title, job_desc)
-
+    try:
+        position_level = handle_position_level(title, job_desc)
+    except:
+        position_level = None
     # Handling the "languages" field
-    position_languages = handle_languages(title, job_desc)
-
+    try:
+        position_languages = handle_languages(title, job_desc)
+    except:
+        position_languages = None
     # Handling the "technologies" list
-    position_technologies = handle_technologies(title, job_desc)
-
+    try:
+        position_technologies = handle_technologies(title, job_desc)
+    except:
+        position_technologies = None
     # print(job_desc)
 
-    offer = pass_vars_to_dict(
+    return pass_vars_to_dict(
         title,
         company,
         company_size,
@@ -399,15 +422,15 @@ def scrap(
         b2b,
         uop,
         offer_link,
+        offer_age,
         active
     )
 
-    return offer
 
 
 def handle_contracts(title, job_desc):
     contracts = find_words_in_text(title, contracts_list)
-    if not contracts:
+    if not contracts and job_desc != None:
         contracts = find_words_in_text(job_desc, contracts_list)
     if not contracts:
         contracts = None
@@ -427,11 +450,12 @@ def pass_vars_to_dict(
         b2b,
         uop,
         offer_link,
+        offer_age,
         active):
 
     offer = {"title": title,
              "company": company,
-             "company_size": company_size if company_size else None,
+             "company_size": int(company_size[0]) if company_size else None,
              "location": {
                  "address": location,
              },
@@ -443,7 +467,7 @@ def pass_vars_to_dict(
                      "b2b": True if (contracts and "b2b" in contracts) or b2b else False,
                      "uop": True if (contracts and "uop" in contracts) or uop else False,
                  },
-                 "salary:": {
+                 "salary": {
                      "b2b": {
                          "min": b2b[0] if b2b else None,
                          "max": b2b[1] if b2b else None
@@ -456,9 +480,10 @@ def pass_vars_to_dict(
              }
              }
 
-    offer["hash"] = __generate_offer_hash(offer)
+    offer["offer_hash"] = __generate_offer_hash(offer)
     offer["offer_link"] = offer_link
-    offer["source_page"] = "indeed"
+    offer["source_page"] = "indeed.com"
+    offer["date"] = date.today() - timedelta(days=offer_age) if offer_age != None else date.today() - timedelta(days=30)
     offer["active"] = active
 
     return offer
@@ -508,6 +533,8 @@ def handle_position_level(title, job_desc):
 
 
 def find_company_size(regex_formula_company_size, job_desc):
+    if job_desc == None:
+        return None
     return re.findall(r"\d+",
                       find_regex(
                           re.finditer(
@@ -516,9 +543,9 @@ def find_company_size(regex_formula_company_size, job_desc):
                       )
 
 
-def save_to_json(data_json):
-    with open("indeed.json", "w", encoding="utf-8") as f:
-        json.dump(data_json, f, ensure_ascii=False, indent=4)
+def save_to_json(data_json, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data_json, f, ensure_ascii=False, separators=(',', ':'), default=str)
 
 
 def decide_where_to_pass_min_max(min, max, uop, b2b, contracts):
@@ -526,9 +553,9 @@ def decide_where_to_pass_min_max(min, max, uop, b2b, contracts):
         if "b2b" in contracts and not uop:
             if "uop" in contracts:
                 return uop, b2b
-            return uop, [min, max]
+            return uop, [int(min), int(max)]
         elif "uop" in contracts and not uop:
-            return [min, max], b2b
+            return [int(min), int(max)], b2b
 
     return uop, b2b
 
